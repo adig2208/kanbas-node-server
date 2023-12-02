a511c7d5a7b2ad1e8aa83136d92a64cd28893c5c
adityagurnani22 <gurnani.ad@northeastern.edu>
2023-12-01 22:24:50 -0500

    Mongo setup

---
+++ app.js
@@ -0,0 +1,39 @@
+import "dotenv/config";
+import express from 'express'
+import session from "express-session";
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+mongoose.connect(CONNECTION_STRING);
+import Hello from "./hello.js"
+import Lab5 from './Lab5.js'
+import CourseRoutes from "./courses/routes.js";
+import UserRoutes from "./users/routes.js";
+import ModuleRoutes from "./modules/routes.js";
+import AssignmentRoutes from "./assignments/routes.js";
+import cors from "cors"
+const app = express()
+app.use(cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+}));
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+};
+if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+        sameSite: "none",
+        secure: true,
+    };
+}
+app.use(session(sessionOptions));
+app.use(express.json());
+CourseRoutes(app);
+ModuleRoutes(app);
+AssignmentRoutes(app);
+UserRoutes(app);
+Hello(app)
+Lab5(app)
+app.listen(process.env.PORT || 4000);
